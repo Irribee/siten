1976d32315117bf2be643d771ef2d4fdee8e7114
Irribee <stelirivik@mail.ru>
2021-08-05 20:44:23 +0300

    Learning 1

---
+++ Learning/src/MassivMinimum.java
@@ -0,0 +1,13 @@
+public class MassivMinimum {
+        public static void main(String[] args) {
+            int[] massivMin = {6, 18, 3, 76, 32, 14};
+            for (int i=0; i<massivMin.length; i++)
+                int minimal= massivMin[0];
+            do {
+                int i ++;
+            }
+            while (int i = 0);
+            System.out.println("Минимальное число массива" + i+1);
+        }
+    }
+}
---
+++ Learning/src/Square.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+public class ploshad {
+    public static void main(String[] args) {
+        Scanner name = new Scanner(System.in);
+        do {
+            System.out.println("Площадь какой фигуры вы хотите рассчитать");
+            String figura = name.nextLine();
+            switch (figura) {
+                case "треугольник":
+                    System.out.println("основание треугольника равно");
+                    int a = name.nextInt();
+                    System.out.println("высота треугольника равна");
+                    int h = name.nextInt();
+                    int s = (a * h) / 2;
+                    System.out.println("площадь треугольника равна" + s);
+                    break;
+                case "квадрат":
+                    System.out.println("сторона квадрата равна");
+                    int c = name.nextInt();
+                    int pl = c ^ 2;
+                    System.out.println("площадь квадрата равна" + pl);
+                    break;
+                case "прямоугольник":
+                    System.out.println("длина прямоугольника равна");
+                    int n = name.nextInt();
+                    System.out.println("ширина прямоугольника равна");
+                    int m = name.nextInt();
+                    int plos = n * m;
+                    System.out.println("площадь прямоугольника равен" + plos);
+                    break;
+                case "круг":
+                    System.out.println("адиус круга равен");
+                    double r = name.nextInt();
+                    double plosh = 3.14 * r * r;
+                    System.out.println("площадь круга равна" + plosh);
+                    break;
+                case "не хочу больше":
+                    System.out.println("На сегодня хватит");
+                    String topslovo = name.nextLine();
+                    break;
+            }
+            while (!figura.equals("стоп")) ;
+            System.out.println("Всего доброго");
+        }
+    }
+}
