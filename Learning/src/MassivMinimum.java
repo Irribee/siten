0de47383641eaaf8c88565d591a3ed0c59bd3322
Irribee <stelirivik@mail.ru>
2021-08-07 23:55:53 +0300

    Update MassivMinimum.java

--- Learning/src/MassivMinimum.java
+++ Learning/src/MassivMinimum.java
@@ -1,8 +1,14 @@
 public class MassivMinimum {
     public static void main(String[] args) {
-        int[] massivMin = {6, 18, 3, 76, 32, 14};
+        int[] massivMin = {6, 18, 3, 76, 2, 32, 14};
         for (int i = 0; i < massivMin.length; i++) {
-
+            if (massivMin[i]<massivMin [i+1]) {
+                i++;
+            }
+            else if (massivMin[i]>massivMin [i+1]){
+                int massivMin [i+1] =massivMin[i];
+            }
+            System.out.println("Минимальное число массива - "+ massivMin [i] );
         }
     }
 }
