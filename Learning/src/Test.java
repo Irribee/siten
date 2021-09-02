8ab7e4243c5994b0024e5483c1fdadf888055819
Irribee <stelirivik@mail.ru>
2021-09-02 22:03:36 +0300

    Update Test.java

--- Learning/src/Test.java
+++ Learning/src/Test.java
@@ -1,54 +1,46 @@
-class Test {
-    public static boolean substringInString(String string, String subst) {
-        //тут я должна использовать условие со string и subst
-        return true;
-            else return false;
-    }
-
-    private char[] toCharArray() {
-        return new char[0];
-    }
-
+public class Test {
     public static void main(String[] args) {
-        substringInString("Hello", "lo");
-        Test string = new Test();
-        Test subst = new Test();
-        char[] str = string.toCharArray();
-        char[] sub = subst.toCharArray();
-        int i;
-        int k;
-        int t = 0;
-        for (i = 0; i < str.length; i++) {
-            if (str[i] != sub[0]) {
-                continue;
-            } else if (i + 1 == str.length) {
-                break;
-            } else if (str[i] == sub[0]) {
-                t++;
-                i++;
-                for (k = 1; k < sub.length; k++) {
-                    if (i == str.length) {
+        SubInStr element1 = new SubInStr();
+        element1.substringInString("Hello", "lo");
+        SubInStr element2 = new SubInStr();
+        element2.substringInString("Hello", "lo");
+        }
+    }
+        class SubInStr {
+            int i = 0;
+            int k;
+            int t = 0;
+
+            public boolean substringInString(String string, String subst) {//тут я должна использовать условие со string и subst
+                char[] str = string.toCharArray();
+                char[] sub = subst.toCharArray();
+                for (i = 0; i < str.length; i++) {
+                    if (str[i] != sub[0]) {
+                        continue;
+                    } else if (i + 1 == str.length) {
                         break;
-                    } else if (str[i] != sub[k]) {
-                        t = 0;
-                        i = i - k;
-                        break;
-                    } else if (str[i] == sub[k]) {
+                    } else if (str[i] == sub[0]) {
                         t++;
                         i++;
+                        for (k = 1; k < sub.length; k++) {
+                            if (i == str.length) {
+                                break;
+                            } else if (str[i] != sub[k]) {
+                                t = 0;
+                                i = i - k;
+                                break;
+                            } else if (str[i] == sub[k]) {
+                                t++;
+                                i++;
+                            }
+                        }
                     }
                 }
+                boolean result = (t >= sub.length);
+                if (result)
+                    return true;
+                else
+                    return false;
+
             }
         }
-        if (t >= sub.length) {
-            // System.out.println("true");
-        }
-        if (t < sub.length) {
-            // System.out.println("false");
-        }
-//            if (string.substringInString("Hello", "lo"));
-//            System.out.println("the substring "+subst+" is contained in the string"+str);
-//            if (string.substringInString("Hello", "www"));
-//            System.out.println("the substring "+sub+" is not contained in the string"+str);;
-    }
-}
