bdf96d252632669e664b501eec481052d70ac6d1
Irribee <stelirivik@mail.ru>
2021-09-02 22:12:00 +0300

    Update Test.java

--- Learning/src/Test.java
+++ Learning/src/Test.java
@@ -6,43 +6,43 @@ public class Test {
         SubInStr element2 = new SubInStr();
         if (element2.substringInString("Hello", "ww"))
             System.out.println("true");
-        }
     }
-        class SubInStr {
-            int i = 0;
-            int k;
-            int t = 0;
+}
 
-            public boolean substringInString(String string, String subst) {//тут я должна использовать условие со string и subst
-                char[] str = string.toCharArray();
-                char[] sub = subst.toCharArray();
-                for (i = 0; i < str.length; i++) {
-                    if (str[i] != sub[0]) {
-                        continue;
-                    } else if (i + 1 == str.length) {
+class SubInStr {
+    int i = 0;
+    int k;
+    int t = 0;
+
+    public boolean substringInString(String string, String subst) {//тут я должна использовать условие со string и subst
+        char[] str = string.toCharArray();
+        char[] sub = subst.toCharArray();
+        for (i = 0; i < str.length; i++) {
+            if (str[i] != sub[0]) {
+                continue;
+            } else if (i + 1 == str.length) {
+                break;
+            } else if (str[i] == sub[0]) {
+                t++;
+                i++;
+                for (k = 1; k < sub.length; k++) {
+                    if (i == str.length) {
                         break;
-                    } else if (str[i] == sub[0]) {
+                    } else if (str[i] != sub[k]) {
+                        t = 0;
+                        i = i - k;
+                        break;
+                    } else if (str[i] == sub[k]) {
                         t++;
                         i++;
-                        for (k = 1; k < sub.length; k++) {
-                            if (i == str.length) {
-                                break;
-                            } else if (str[i] != sub[k]) {
-                                t = 0;
-                                i = i - k;
-                                break;
-                            } else if (str[i] == sub[k]) {
-                                t++;
-                                i++;
-                            }
-                        }
                     }
                 }
-                boolean result = (t >= sub.length);
-                if (result)
-                    return true;
-                else
-                    return false;
-
             }
         }
+        boolean result = (t >= sub.length);
+        if (result)
+            return true;
+        else
+            return false;
+    }
+}
