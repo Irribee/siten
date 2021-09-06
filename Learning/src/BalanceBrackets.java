7dafb6d08bc990b0383deb54f91b7e3a6d9cef9c
Irribee <stelirivik@mail.ru>
2021-09-07 00:15:35 +0300

    Create BalanceBrackets.java

---
+++ Learning/src/BalanceBrackets.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class BalanceBrackets {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter open and closed curly braces");
+        String string = scan.nextLine();
+        char[] brackets = string.toCharArray();
+        int i;
+        int k = 0;
+        for (i = 0; i < brackets.length; i++) {
+            switch (string) {
+                case "{":
+                    k++;
+                    break;
+                case "}":
+                    k--;
+                    break;
+            }
+        }
+        if (k == 0) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+    }
+}
+
+
