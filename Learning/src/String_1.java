d5a7a7bf70abeaf866adad9fe762abba1a9adb41
Irribee <stelirivik@mail.ru>
2021-10-20 00:33:24 +0300

    Update String_1.java

--- Learning/src/String_1.java
+++ Learning/src/String_1.java
@@ -5,30 +5,34 @@ public class String_1 {
 //seeColor("xxred") → ""
 //seeColor("blueTimes") → "blue"
     public String seeColor(String str) {
-        if (str.length() >= 3 && str.substring(0, 3).equals("red")) {
+        if (str.length()>=3 && str.startsWith("red")){
             return "red";
-        } else if (str.length() >= 4 && str.substring(0, 4).equals("blue")) {
+        }
+        else if (str.length()>=4 && str.startsWith("blue")){
             return "blue";
-        } else {
+        }
+        else{
             return "";
         }
     }
-}
 
     //    Given a string, if one or both of the first 2 chars is 'x', return the string without those 'x' chars, and otherwise return the string unchanged. This is a little harder than it looks.
 //        withoutX2("xHi") → "Hi"
 //        withoutX2("Hxi") → "Hi"
 //        withoutX2("Hi") → "Hi"
     public String withoutX2(String str) {
-        if (str.length() > 1 && str.substring(0, 2).equals("xx")) {
-            return str.substring(2, str.length());
-        } else if (str.length() > 0 && str.substring(0, 1).equals("x")) {
-            return str.substring(1, str.length());
-        } else if (str.length() > 0 && str.substring(1, 2).equals("x")) {
-            String left = str.substring(0, 1);
-            String right = str.substring(2, str.length());
-            return left + right;
-        } else {
+        if (str.length()>1 && str.charAt(0)=='x' && str.charAt(1)=='x'){
+            return str.substring(2,str.length());
+        }
+        else if (str.length()>0 && str.charAt(0)=='x'){
+            return str.substring(1,str.length());
+        }
+        else if (str.length()>0 && str.charAt(1)=='x'){
+            String left = str.substring(0,1);
+            String right=str.substring(2,str.length());
+            return left+right;
+        }
+        else{
             return str;
         }
     }
