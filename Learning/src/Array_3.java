3260beae62d7023db63e9f6b8c9b1ea861626194
Irribee <stelirivik@mail.ru>
2021-11-10 00:31:40 +0300

    Update Array_3.java

--- Learning/src/Array_3.java
+++ Learning/src/Array_3.java
@@ -5,6 +5,26 @@ public class Array_3 {
 //    maxSpan([1, 2, 1, 1, 3]) → 4
 //    maxSpan([1, 4, 2, 1, 4, 1, 4]) → 6
 //    maxSpan([1, 4, 2, 1, 4, 4, 4]) → 6
+public int maxSpan(int[] nums) {
+    int span=0;
+    if (nums.length==1){
+        span=1;
+    }
+    for (int i=0; i<nums.length;i++){
+        for (int j=nums.length-1; j>i;j--){
+            if (nums[i]==nums[j] && span<j-i){
+                span=j-i+1;
+            }
+            else if (nums[i]==nums[j] && span>j-i){
+                span=span;
+            }
+        }
+    }
+    if(span==0 && nums.length!=0 ){ //спросить у Пашки
+        return 1;
+    }
+    return span;
+}
 
 
 //    Given a non-empty array, return true if there is a place to split the array so that the sum of the numbers on one side is equal to the sum of the numbers on the other side.
@@ -38,6 +58,17 @@ public class Array_3 {
 //    seriesUp(4) → [1, 1, 2, 1, 2, 3, 1, 2, 3, 4]
 //    seriesUp(2) → [1, 1, 2]
 
+    public int[] seriesUp(int n) {
+        int[] arr=new int[n*(n + 1)/2];
+        int index=0;
+        for(int i=1;i<n+1;i++){
+            for(int j=0;j<i;j++){
+                arr[index]=j+1;
+                index++;
+            }
+        }
+        return arr;
+    }
 
 
 }
