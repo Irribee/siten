4efdc8eff2810227555277f519ce10fc0ed15240
Irribee <stelirivik@mail.ru>
2021-11-28 21:29:28 +0300

    subst

--- Learning/src/String_3.java
+++ Learning/src/String_3.java
@@ -165,6 +165,31 @@ public class String_3 {
 //    notReplace("is-is") → "is not-is not"
 //    notReplace("This is right") → "This is not right"
 
+    public String notReplace(String str) {
+        String strNot = "";
+        for (int i = 0; i < str.length(); i++) {
+            if (str.length() == 2 && str.substring(i, i + 2).equals("is")) {
+                return str.substring(i, i + 2) + " not";
+            }
+            if (i == str.length() - 3 && str.substring(i, i + 3).equals(" is")) {
+                strNot += str.substring(i, i + 3) + " not";
+                break;
+            }
+            if (Character.isLetter(str.charAt(i)) == false && str.substring(i - 2, i).equals("is")) {
+                if (i > 2 && Character.isLetter(str.charAt(i - 3)) == false) {
+                    strNot += " not";
+                }
+                if (i == 2) {
+                    strNot += " not ";
+                } else {
+                    strNot += str.substring(i, i + 1);
+                }
+            } else {
+                strNot += str.substring(i, i + 1);
+            }
+        }
+        return strNot;
+    }
 
 //    Given a string, return true if the number of appearances of "is" anywhere in the string is equal to the number of appearances of "not" anywhere in the string (case sensitive).
 //
