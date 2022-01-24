128cb65e3103fe45922afeb1e329a86e25ddbd8a
Irribee <stelirivik@mail.ru>
2022-01-24 09:10:07 +0300

    The square and perimetr

--- OOP/src/Circle.java
+++ OOP/src/Circle.java
@@ -4,12 +4,12 @@ public class Circle implements SquareAndPerimetr {
     double p;
 
     public void Square() {
-        s = (double) 3.14 * r * r;
+        s = 3.14 * r * r;
         System.out.println("The square of the Circle= " + s);
     }
 
     public void Perimetr() {
-        p = (double) 2 * 3.14 * r;
+        p = 2 * 3.14 * r;
         System.out.println("The perimetr of the Circle= " + p);
     }
 }
--- OOP/src/Triangle.java
+++ OOP/src/Triangle.java
@@ -3,16 +3,16 @@ public class Triangle implements SquareAndPerimetr {
     int b = 9;
     int c = 5;
     int h = 18;
-    double p;
+    int p;
     double s;
 
     public void Square() {
-        s = (double) (a * h) / 2;
+        s = (a * h) / 2;
         System.out.println("The square of the triangle= " + s);
     }
 
     public void Perimetr() {
-        p = (double) a + b + c;
+        p = a + b + c;
         System.out.println("The perimetr of the triangle= " + p);
     }
 }
