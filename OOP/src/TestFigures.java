e18509be4e0cfa4b178e7f9faba2cd60c3b8eaa0
Irribee <stelirivik@mail.ru>
2022-01-28 23:49:34 +0300

    The square and perimetr

--- OOP/src/TestFigures.java
+++ OOP/src/TestFigures.java
@@ -1,12 +1,13 @@
 public class TestFigures {
+
     public static void main(String[] args) {
+        int sumSquare = 0;
+        int sumPerimeter = 0;
         SquareAndPerimeter[] figures = new SquareAndPerimeter[4];
         figures[0] = new Triangle(4, 10, 7, 15);
         figures[1] = new Circle(19);
         figures[2] = new Foursquare(47);
         figures[3] = new Trapezoid(4, 6, 8, 23, 11);
-        int sumSquare = 0;
-        int sumPerimeter = 0;
         for (int i = 0; i < figures.length; i++) {
             sumSquare += figures[i].square();
             sumPerimeter += figures[i].perimeter();
@@ -15,3 +16,5 @@ public class TestFigures {
         System.out.println("the sum of the perimeters of all figures = " + sumSquare);
     }
 }
+
+
