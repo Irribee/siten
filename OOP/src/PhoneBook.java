98854aa8871ec0bba3adcfa4eaa8c97ad6edae06
Irribee <stelirivik@mail.ru>
2022-03-02 00:38:51 +0300

    Create PhoneBook.java

---
+++ OOP/src/PhoneBook.java
@@ -0,0 +1,40 @@
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
+
+public class PhoneBook {
+    public static void main(String[] args) {
+        TreeMap<String, Long> phoneBook = new TreeMap<>();
+        phoneBook.put("Rodion", 89536975212L);
+        phoneBook.put("Anton", 89680369941L);
+        phoneBook.put("Katya", 89543695665L);
+        phoneBook.put("Borya", 84563697741L);
+        phoneBook.put("Nina", 89776589631L);
+        Scanner scanner = new Scanner(System.in);
+        for (; ; ) {
+            System.out.println("Input the name: ");
+            String name = scanner.nextLine();
+            long phoneNumber = 0;
+            if (phoneBook.containsKey(name)) {
+                phoneNumber = phoneBook.get(name);
+                System.out.println(name + " : " + phoneNumber);
+            }
+            else if (name.equals("End")) {
+                printMap(phoneBook);
+                continue;
+            }
+            else if (!phoneBook.containsKey(name)) {//где то в этом условии ошибка
+                System.out.println("Input the phone number: ");
+                phoneNumber = scanner.nextLong();
+                System.out.println(name + " : " + phoneNumber);
+                phoneBook.put(name, phoneNumber);
+            }
+        }
+    }
+    public static void printMap(Map<String, Long> map) {
+        for (String key : map.keySet()) {
+            System.out.println(key + " - " + map.get(key));
+        }
+    }
+}
+
