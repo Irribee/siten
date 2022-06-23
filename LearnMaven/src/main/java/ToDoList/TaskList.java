54b4b9b6be441acf51377c5083c08109b0f4d98b
Irribee <stelirivik@mail.ru>
2022-06-24 01:37:39 +0300

    начало

---
+++ LearnMaven/src/main/java/ToDoList/TaskList.java
@@ -0,0 +1,34 @@
+package ToDoList;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class TaskList {
+
+    HashMap<Integer, Task> taskList = new HashMap<Integer, Task>();
+
+    public boolean addTask(int idTask, Task textTask) {
+        taskList.put(idTask, textTask);
+        return true;
+    }
+
+    public Task getTask(int taskId) {
+        Task desiredTask = taskList.get(taskId);
+        return desiredTask;
+    }
+
+    public void removeTask(int idRemove) {
+        taskList.remove(idRemove);
+
+    }
+
+    //    public void printTaskList(Map<Integer, Task> map) {
+//        for (Integer key: map.keySet()){
+//        System.out.println(key+" "+map.get(key);
+//    }
+    public void printTaskList() {
+        System.out.println(taskList);
+    }
+}
---
+++ LearnMaven/src/test/java/testToDoList/ConsoleToDo.java
@@ -0,0 +1,56 @@
+package testToDoList;
+
+import ToDoList.Task;
+import ToDoList.TaskList;
+
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+class ConsoleTodo {
+    public static void main(String[] args) {
+        TaskList taskList = new TaskList();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Здравствуйте");
+        for (; ; ) {
+            System.out.println("Выберите действие: \nДобавить задачу\nУдалить задачу\nИзменить статус задачи\nВывести весь список задач\nВыполнить отчет по задачам");
+            String choice = scanner.nextLine();
+            switch (choice) {
+                case "Добавить задачу":
+                    System.out.println("Напишите текст задачи:");
+                    String textTask = scanner.nextLine();
+                    System.out.println("Введите дату, к которой задача должна быть выполнена:");
+                    System.out.println("год");
+                    int year = scanner.nextInt();
+                    System.out.println("месяц");
+                    int month = scanner.nextInt();
+                    System.out.println("число");
+                    int date = scanner.nextInt();
+                    System.out.println("Введите id задачи");
+                    int idTask = scanner.nextInt();
+                    Task task = new Task();
+                    task.setStatus("новая");
+                    task.setDate(year, month, date);
+                    task.setText(textTask);
+                    taskList.addTask(idTask, task);
+                    break;
+                case "Удалить задачу":
+                    System.out.println("Введите id задачи, которую вы хотите удалить");
+                    int idDelete = scanner.nextInt();
+                    //taskList.getTask(taskDelete);
+                    taskList.removeTask(idDelete);
+                    break;
+                case "Изменить статус задачи":
+                    System.out.println("Введите id задачи,статус которой вы хотели бы изменить");
+                    int idChangeStatus = scanner.nextInt();
+                    System.out.println("Введите новый статус задачи:\n в работе \n сделано");
+                    String newStatus = scanner.nextLine();
+                    taskList.getTask(idChangeStatus).setStatus(newStatus);
+                    break;
+                case "Вывести весь список задач":
+                    taskList.printTaskList();
+                    break;
+            }
+
+        }
+    }
+}
